69cc3f80f68d59af42f7d3bb2c5ed799c85a3937
DuyThao <duythao@bacoor.co>
2021-12-29 14:19:39 +0700

    Approve

--- approve.js
+++ approve.js
@@ -1,26 +1,26 @@
-const { BigNumber } = require('@ethersproject/bignumber')
+require('dotenv').config();
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.RPC));
 const accounts = require('./accounts.json');
 const tokenABI = require('./ABI/ERC20.json');
-const nftABI = require('./ABI/ERC20.json')
+const nftABI = require('./ABI/ERC721.json')
 
-const tokenAddress = '';
+const tokenAddress = '0xdCe0cF324155F6331bA1Bb1957b47d80d107Eb5D';
 const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
-const spender = '';
+const spender = '0xfd404Faadb1FE9A1D08c74F2711D92FaBbDC7433';
 
 const nftAddress = '';
 const nftContract = new web3.eth.Contract(nftABI, nftAddress);
-const MaxUint256 = (BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+const MaxUint256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
 const defaultWei = 5000000000; // default 5 GWei
 const gasPrice = defaultWei * 1.1;
 
 const approveToken = async (account, privateKey) => {
   try {
-    const dataTx = tokenContract.methods.approve(spender, MaxUint256._hex).encodeABI();
-
+    const dataTx = tokenContract.methods.approve(spender, MaxUint256).encodeABI();
     const nonce = await web3.eth.getTransactionCount(account);
+    
     const rawTransaction = {
       nonce: web3.utils.toHex(nonce),
       from: account,
@@ -32,13 +32,13 @@ const approveToken = async (account, privateKey) => {
     const gasLimit = await web3.eth.estimateGas(rawTransaction);
     const gasLimitHex = web3.utils.toHex(gasLimit);
     rawTransaction.gasLimit = gasLimitHex;
-
+    
     const signedTransaction= await web3.eth.accounts.signTransaction(rawTransaction, privateKey);
     
     return web3.eth
       .sendSignedTransaction(signedTransaction.rawTransaction)
       .on('receipt', ({ transactionHash }) => {
-        console.log(transactionHash);
+        console.log(`${process.env.EXPLORER}/tx/${transactionHash}`);
       })
       .catch((err) => {
         console.log('error1', err);
@@ -70,7 +70,7 @@ const approveNFT = async (account, privateKey) => {
     return web3.eth
       .sendSignedTransaction(signedTransaction.rawTransaction)
       .on('receipt', ({ transactionHash }) => {
-        console.log(transactionHash);
+        console.log(`${process.env.EXPLORER}/tx/${transactionHash}`);
       })
       .catch((err) => {
         console.log('error1', err);
@@ -85,7 +85,7 @@ const script = async () => {
   for (let i = 0; i < length; i++) {
     const { address, privateKey } = accounts[i];
     await approveToken(address, privateKey);
-    await approveNFT(address, privateKey);
+    // await approveNFT(address, privateKey);
   }
 };
 
